c8fd2677c18b0a3136f2b7fbf7b133ce3d5a0dbf
sohui.dev <113837605+qwerty00ui88@users.noreply.github.com>
2024-11-22 14:35:04 +0900

    config: morgan, helmet 추가

--- server/app.js
+++ server/app.js
@@ -3,20 +3,25 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import rootRouter from './router/root.js';
 import tweetsRouter from './router/tweets.js';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// 전역 설정
-app.use(
-  cors({
-    origin: [`${process.env.CLIENT_BASE_URL}`],
-    credentials: true,
-  })
-);
+const corsOption = {
+  origin: [`${process.env.CLIENT_BASE_URL}`],
+  credentials: true,
+};
+
+// 미들웨어
+app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(morgan('combined'));
+app.use(helmet());
 
 // 라우터
 app.use('/', rootRouter).use('/tweets', tweetsRouter);
@@ -31,4 +36,5 @@ app.use((error, req, res, next) => {
   res.status(500).send('Server Error');
 });
 
-app.listen(3000);
+// 서버 실행
+app.listen(PORT);
